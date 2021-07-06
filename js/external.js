f83d7148822af2cdb282506281e8a756e22107ea
patrickchiafullo <patrick.chiafullo@gmail.com>
2021-07-06 11:51:25 -0500

    html and javascript exercises

--- js/external.js
+++ js/external.js
@@ -17,12 +17,13 @@
 
 // prompt
 
-var currentSpeedInMph = 65;
-var plannedSpeedInMph = prompt("How fast do you wanna go?");
-var distanceLeftInMiles = prompt("How many miles are ya from where you going?");
-var timeToDestinationAtCurrentSpeed = (distanceLeftInMiles/currentSpeedInMph) * 60;
-var timetoDestinationAtPlannedSPeed = (distanceLeftInMiles/plannedSpeedInMph) * 60;
-var timeGain = timeToDestinationAtCurrentSpeed - timetoDestinationAtPlannedSPeed;
-var minutesGained = Math.floor(timeGain);
-var seconds = Math.round((timeGain - minutesGained) * 60);
-console.log("if you drive at " + plannedSpeedInMph + " mph, you will arrive " + minutesGained + " minutes and " + seconds + " seconds earlier. Is it worth speeding?");
+// var currentSpeedInMph = 65;
+// var plannedSpeedInMph = prompt("How fast do you wanna go?");
+// var distanceLeftInMiles = prompt("How many miles are ya from where you going?");
+// var timeToDestinationAtCurrentSpeed = (distanceLeftInMiles/currentSpeedInMph) * 60;
+// var timetoDestinationAtPlannedSPeed = (distanceLeftInMiles/plannedSpeedInMph) * 60;
+// var timeGain = timeToDestinationAtCurrentSpeed - timetoDestinationAtPlannedSPeed;
+// var minutesGained = Math.floor(timeGain);
+// var seconds = Math.round((timeGain - minutesGained) * 60);
+// console.log("if you drive at " + plannedSpeedInMph + " mph, you will arrive " + minutesGained + " minutes and " + seconds + " seconds earlier. Is it worth speeding?");
+
