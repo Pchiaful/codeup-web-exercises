f373e66d34376928028a10f156d8d4365579afb0
patrickchiafullo <patrick.chiafullo@gmail.com>
2021-07-06 11:19:16 -0500

    hmtl practice and notes

---
+++ js/external.js
@@ -0,0 +1,28 @@
+"use strict";
+
+// alerts
+
+// alert("Hi, I am a JavaScript Alert!");
+
+// confirm
+
+// var userDecision = confirm("Wanna buy some deathsticks?");
+// console.log(userDecision);
+//
+// if (userDecision === true){
+//     alert("Smart!");
+// } else {
+//     alert("Oh boo...");
+// }
+
+// prompt
+
+var currentSpeedInMph = 65;
+var plannedSpeedInMph = prompt("How fast do you wanna go?");
+var distanceLeftInMiles = prompt("How many miles are ya from where you going?");
+var timeToDestinationAtCurrentSpeed = (distanceLeftInMiles/currentSpeedInMph) * 60;
+var timetoDestinationAtPlannedSPeed = (distanceLeftInMiles/plannedSpeedInMph) * 60;
+var timeGain = timeToDestinationAtCurrentSpeed - timetoDestinationAtPlannedSPeed;
+var minutesGained = Math.floor(timeGain);
+var seconds = Math.round((timeGain - minutesGained) * 60);
+console.log("if you drive at " + plannedSpeedInMph + " mph, you will arrive " + minutesGained + " minutes and " + seconds + " seconds earlier. Is it worth speeding?");
