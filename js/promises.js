d7de789e0906e94d13c253773e468f0df609afd1
patrickchiafullo <patrick.chiafullo@gmail.com>
2021-08-30 17:32:03 -0500

    promises exercise complete

---
+++ js/promises.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// const KEYLINK = PERSONAL_PROMISES_KEY;
+
+const GITHUBLINK = 'https://api.github.com/${username}/events/';
+
+// "https://api.github.com/search/commits?q={query}{&page,per_page,sort,order}"
+
+const userName = {
+    username: 'username'
+}
+
+const options = {
+    method: 'Get',
+    headers: {
+        'Authorization': 'token ${KEYLINK}'
+    },
+    path: JSON.stringify(userName),
+};
+
+fetch(GITHUBLINK, options)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data[1].created_at)
+    })
+    .catch(console.error)
+
+
+
+
+// fetch(GITHUBLINK)
+//     .then(response => response.json())
+//     .then((data) => console.log(data.))
+//     .catch(console.error)
+
+
+//
+// fetch(GITHUBLINK, options)
+//     .then(response => response.json)
+//     .then(console.log)
+//     .catch(console.error)
